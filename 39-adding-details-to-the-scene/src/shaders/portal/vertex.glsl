9893f059e852d19b3c65377dc34ac15426a6a3ce
Joe D <joedark03@gmail.com>
2023-05-12 20:41:25 -0400

    code for lesson 39

---
+++ 39-adding-details-to-the-scene/src/shaders/portal/vertex.glsl
@@ -0,0 +1,11 @@
+
+varying vec2 vUv;
+void main(){
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectionPostiton = projectionMatrix * viewPosition;
+
+    gl_Position = projectionPostiton;
+
+    vUv = uv;
+}
